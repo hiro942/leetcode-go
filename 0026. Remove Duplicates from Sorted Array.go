170fe92221dfc862b09825563c26994fdca96a78
hiro942 <ee.Liulei@Gmail.com>
2022-08-07 01:19:03 +0800

    update

---
+++ 0026. Remove Duplicates from Sorted Array.go
@@ -0,0 +1,12 @@
+package leetcode
+
+func removeDuplicates(nums []int) int {
+	l := 0
+	for i := 0; i < len(nums); i++ {
+		if i == 0 || nums[i] != nums[i-1] {
+			nums[l] = nums[i]
+			l++
+		}
+	}
+	return l
+}
