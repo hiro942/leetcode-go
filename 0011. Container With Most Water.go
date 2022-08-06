873553d4812623e6fe95e06604b6dbd229a167dc
hiro942 <ee.Liulei@Gmail.com>
2022-08-06 15:34:53 +0800

    update

---
+++ 0011. Container With Most Water.go
@@ -0,0 +1,21 @@
+package leetcode
+
+func maxArea(height []int) (ans int) {
+	for i, j := 0, len(height)-1; i < j; {
+		if height[i] <= height[j] {
+			ans = max(ans, (j-i)*height[i])
+			i++
+		} else {
+			ans = max(ans, (j-i)*height[j])
+			j--
+		}
+	}
+	return ans
+}
+
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
