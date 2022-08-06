f08b179d2b90a3d02e5a8f14da2249d7dbde4562
hiro942 <ee.Liulei@Gmail.com>
2022-08-07 01:24:42 +0800

    update

---
+++ 0027. Remove Element.go
@@ -0,0 +1,12 @@
+package leetcode
+
+func removeElement(nums []int, val int) int {
+	l := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != val {
+			nums[l] = nums[i]
+			l++
+		}
+	}
+	return l
+}
