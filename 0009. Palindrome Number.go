1bdbb3ce4651c63ec52f04c39928d96a6116b27a
hiro942 <ee.Liulei@Gmail.com>
2022-08-06 13:52:08 +0800

    update

--- 0009. Palindrome Number.go
+++ 0009. Palindrome Number.go
@@ -1,27 +1,18 @@
 package leetcode
 
-import "strconv"
-
 func isPalindrome(x int) bool {
-	if x < 0 {
+	if x < 0 || x > 0 && x%10 == 0 {
 		return false
 	}
 
-	xs := strconv.Itoa(x)
-	n := len(xs)
-	var i, j int
-	if n%2 == 0 {
-		i = n/2 - 1
-		j = n / 2
-	} else {
-		i = n/2 - 1
-		j = n/2 + 1
+	y := 0
+	for y <= x {
+		y = y*10 + x%10
+		if x == y || x/10 == y {
+			return true
+		}
+		x /= 10
 	}
 
-	for ; i >= 0 && j < n; i, j = i-1, j+1 {
-		if xs[i] != xs[j] {
-			return false
-		}
-	}
-	return true
+	return false
 }
